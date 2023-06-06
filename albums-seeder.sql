939a776cacb39ebf3e59dec96a43f77d55ce2642
TyreszB <127250161+TyreszB@users.noreply.github.com>
2023-06-06 16:45:07 -0400

    finished exercise

--- albums-seeder.sql
+++ albums-seeder.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE albums;
+
 INSERT INTO albums(artist,name,release_date,sales,genre)
 VALUES ('Micheal Jackson','Thriller',1982,51.2,'Pop'),
 ('AC/DC','Back in Black',1980,30.1,'Hard Rock'),
